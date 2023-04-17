d0158d75c1c25ef7671a282e71f8c847484a2843
Paritosh Chaudhary <51153681+Paritosh008@users.noreply.github.com>
2023-04-17 14:59:01 +0530

    Create constructor1.sol

---
+++ constructor1.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier:GPL-3.0
+pragma solidity >=0.5.0 <0.9.0;
+
+contract local
+{
+    uint public count;
+
+    constructor(uint new_count)
+    {
+        count = new_count;
+    }
+}
