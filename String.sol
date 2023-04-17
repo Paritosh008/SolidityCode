38c093da7d9c2574dc519887d69943a136820d2a
Paritosh Chaudhary <51153681+Paritosh008@users.noreply.github.com>
2023-04-17 14:46:26 +0530

    Create String.sol

---
+++ String.sol
@@ -0,0 +1,10 @@
+pragma solidity 0.8.12;
+contract paritoshchaudhary
+{
+    string message = "paritoshchaudhary";
+    
+    function printmessage() public returns(string memory)
+    {
+        return message;
+    }
+}
