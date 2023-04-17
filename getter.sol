58a48f28e2f45fe2918ea97947ff411cd1276f87
Paritosh Chaudhary <51153681+Paritosh008@users.noreply.github.com>
2023-04-17 14:57:50 +0530

    Create getter.sol

---
+++ getter.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier:GPL-3.0
+
+pragma solidity >=0.5.0 < 0.9.0;
+
+contract local
+{
+    uint public age=10;
+
+    function getter() public view returns(uint)
+    {
+        uint roll  = 100;
+        return roll;
+    }
+}
