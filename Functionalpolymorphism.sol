e7d9038313cbf54573e158a18159fab1d7ecfb61
Paritosh Chaudhary <51153681+Paritosh008@users.noreply.github.com>
2023-04-20 17:21:30 +0530

    Create Functionalpolymorphism.sol

---
+++ Functionalpolymorphism.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier:GPL-3.0
+pragma solidity ^0.4.19;
+contract HelloFunctionPolymorphism
+{
+
+    function getVariableData(int8 data) public constant returns(int8 output)
+    {
+        return data;
+    }
+
+    function overloadedFunction(int16 data) public constant returns(int16 output)
+    {
+        return data;
+    }
+}
